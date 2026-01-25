3f914c586408455e01e4c80cb2aca424921d90cb
Daffa Arviano <daffa.arviano@outlook.com>
2026-01-25 23:01:47 +0700

    redirect root to health

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,6 +12,9 @@ func main() {
 	http.HandleFunc("/api/products", handler.ProductCollection)
 	http.HandleFunc("/categories/", handler.CategoryItem)
 	http.HandleFunc("/categories", handler.CategoryCollection)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/health", http.StatusTemporaryRedirect)
+	})
 	http.HandleFunc("/health", handler.Health)
 	http.HandleFunc("/openapi.yaml", handler.OpenAPI)
 	http.HandleFunc("/docs", handler.Docs)
